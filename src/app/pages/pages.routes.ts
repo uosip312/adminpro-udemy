c4a8bc57fff6f49382a74116998ac7e224efbbe4
Uosip3 <edison.icr@gmail.com>
2020-04-10 00:19:09 -0400

    Modificacion en las rutas

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+const pagesRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path:'dashboard', component: DashboardComponent },
+            { path:'progress', component: ProgressComponent },
+            { path:'graficas1', component: Graficas1Component },
+            { path:'', redirectTo: '/dashboard', pathMatch: 'full' }
+        ]
+    }
+];
+
+/* export const PAGES_ROUTES = RouterModule.forChild( pagesRoutes ); */
+
+@NgModule({
+    imports: [
+        RouterModule.forChild( pagesRoutes )
+    ],
+    exports: [
+      RouterModule
+    ]
+  })
+
+  export class PAGES_ROUTES { }
